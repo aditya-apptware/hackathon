f9b1718a34517b61685d8357112247330fd2a4b6
Aditya Arakal <aditya.arakal@apptware.com>
2024-12-10 15:57:28 +0530

    updated the header sections texts of the app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,18 @@ export const Header = () => {
   return (
     <header className="flex items-center justify-center bg-[#323232] text-textLight pt-12 pb-8">
       <div className="content text-center py-8 w-full max-w-screen-lg">
-        <div className="title md:w-full w-[50%] m-auto">
-          <h1 className="md:text-3xl text-4xl font-bold">Hackathon is Back</h1>
+        <div className="title md:w-full m-auto text-center">
+          <h1 className="text-[30px] leading-[35px] md:text-[86.2px] md:leading-[100px] font-extrabold">
+            &lt;/HACKATHON
+            <br />
+            IS BACK&gt;
+          </h1>
         </div>
-        <div className="subtitle mt-4 md:w-[300px] w-[80%] m-auto">
-          <p className="md:text-base text-lg">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veniam
+        <div className="subtitle mt-4 m-auto">
+          <p className="text-[16px] md:text-[28px] md:leading-[42px] font-normal">
+            &lt;Lorem ipsum dolor sit, amet consectetur
+            <br />
+            adipisicing elit. Veniam&gt;
           </p>
         </div>
         <div className="register-button mt-8">
