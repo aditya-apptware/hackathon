16bb55bb78a1d8f3eeb1e94e57e78550319f5055
aditya-apptware <aditya.arakal@apptware.com>
2024-12-17 09:31:05 +0530

    updated button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,11 @@ export const Button = ({ label, onClick, className }) => {
   return (
     <button
       onClick={onClick}
-      className={`bg-primary text-textLight py-2 px-4 rounded hover:bg-dark ${className}`}
+      className={
+        className
+          ? className
+          : `bg-primary text-textLight py-2 px-4 rounded hover:bg-dark`
+      }
     >
       {label}
     </button>
