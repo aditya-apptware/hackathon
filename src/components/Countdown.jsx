7e3a723c20cde88b5b8fb6bf84ca31e586575edc
aditya-apptware <aditya.arakal@apptware.com>
2024-12-17 16:38:33 +0530

    countdown

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,65 @@
+import { useEffect, useState } from "react";
+
+const targetDate = new Date("2025-01-17T00:00:00").getTime();
+
+export const Countdown = () => {
+  const [timeRemaining, setTimeRemaining] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = targetDate - now;
+
+      if (distance < 0) {
+        clearInterval(intervalId); // Stop the timer if the target date has passed
+        setTimeRemaining({
+          days: 0,
+          hours: 0,
+          minutes: 0,
+          seconds: 0,
+        });
+      } else {
+        const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        const hours = Math.floor(
+          (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+        );
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        setTimeRemaining({
+          days,
+          hours,
+          minutes,
+          seconds,
+        });
+      }
+    }, 1000); // Update every second
+
+    return () => clearInterval(intervalId); // Cleanup interval on component unmount
+  }, [targetDate]);
+
+  return (
+    <div
+      className="font-robotoMono px-[505px] py-[199px] bg-black text-center"
+      style={{ backgroundImage: "url('../assets/images/countdownbg.svg')" }}
+    >
+      <div className="text-white text-[96px] font-bold tracking-[76.8px] w-[462px] mx-auto">
+        DATE
+      </div>
+      <div className="text-white text-6 mt-[10px]">17th - 19th January</div>
+      <div className="flex gap-[79px] px-[30px] py-[20px] mt-5 font-karmatic text-[#ABD40F] text-[96px] border border-[2px] border-[#ABD40F]">
+        <span>{timeRemaining.days}</span>
+        <span>{timeRemaining.hours}</span>
+        <span>{timeRemaining.minutes}</span>
+      </div>
+      <div className="mt-[30px] text-[#ABD40F]">
+        Registration closes on 16th January 2024 ...
+      </div>
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,9 @@
 import { CodeBuildRepeat } from "../components/CodeBuildRepeat";
-import { Event } from "../components/Event";
-import { Faqs } from "../components/Faqs";
-import { Footer } from "../components/Footer";
+import { Countdown } from "../components/Countdown";
 import { Header } from "../components/Header";
-import { Images } from "../components/Images";
 import { Mentors } from "../components/Mentors";
 import { Participate } from "../components/Participate";
-import { Prizes } from "../components/Prizes";
 import { ProblemStatements } from "../components/ProblemStatements";
-import { Rules } from "../components/Rules";
 
 export const Home = () => {
   return (
@@ -18,8 +13,12 @@ export const Home = () => {
       </div>
       <main className="bg-black">
         <CodeBuildRepeat />
-          <Mentors />
+        <Countdown />
+        <Mentors />
         {/* <div className="py-8 md:py-16">
+       <main>
+        <Countdown />
+       {/*  <div className="py-8 md:py-16">
           <Event />
         </div>
         <div className="py-8 md:py-16 mx-4">
@@ -39,7 +38,7 @@ export const Home = () => {
         <div className="py-8 md:py-16 mt-[-230px] md:pb-[330px] hidden md:block">
           <Images />
         </div> */}
-      </main> 
+      </main>
       {/*  <div>
         <Footer />
       </div> */}
