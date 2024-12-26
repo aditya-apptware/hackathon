1354cd0190df518d1a914d99e3dae65cd901ce1a
aditya-apptware <aditya.arakal@apptware.com>
2024-12-26 11:06:11 +0530

    code build repeat

--- src/components/CodeBuildRepeat.jsx
+++ src/components/CodeBuildRepeat.jsx
@@ -2,13 +2,32 @@ import Box1 from "../assets/images/box1.svg";
 
 export const CodeBuildRepeat = () => {
   return (
-    <div className="relative flex items-start justify-center bg-black pt-[120px] pb-[147px] font-robotoMono">
-      <img src={Box1} alt="box model 1" width={1060} height={570} />
-      <div className="absolute text-white w-[378px] h-[96px] text-[22px] leading-8 capitalize left-[276px] top-[307px]">
+    <div className="relative flex flex-col items-center justify-center bg-black pt-[120px] pb-[147px] font-robotoMono">
+      {/* Responsive Image */}
+      <img
+        src={Box1}
+        alt="box model 1"
+        className="w-full md:w-[1060px] md:h-[570px] object-contain"
+      />
+
+      {/* Left Text */}
+      <div className="absolute text-white w-[90%] sm:w-[378px] h-[96px] text-[22px] leading-8 capitalize left-[5%] sm:left-[276px] top-[15%] sm:top-[307px] text-center sm:text-left hidden md:block">
         &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
         elit libero&gt;
       </div>
-      <div className="absolute text-white w-[407px] h-[96px] text-[22px] leading-8 capitalize right-[324px] bottom-[133px]">
+
+      {/* Right Text */}
+      <div className="absolute text-white w-[90%] sm:w-[407px] h-[96px] text-[22px] leading-8 capitalize right-[5%] sm:right-[324px] bottom-[15%] sm:bottom-[133px] text-center sm:text-right hidden md:block">
+        &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        elit libero&gt;
+      </div>
+
+      {/* Below Text for Smaller Screens */}
+      <div className="md:hidden text-white text-center text-[22px] leading-8 capitalize mt-[20px] mx-auto max-w-[90%] sm:max-w-[378px] sm:max-w-[407px]">
+        &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        elit libero&gt;
+      </div>
+      <div className="md:hidden text-white text-center text-[22px] leading-8 capitalize mt-[20px] mx-auto max-w-[90%] sm:max-w-[378px] sm:max-w-[407px]">
         &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
         elit libero&gt;
       </div>
