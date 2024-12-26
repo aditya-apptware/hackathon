def4c38a2786d6d5a6159a6d6d3d1f0b1b61bad0
aditya-apptware <aditya.arakal@apptware.com>
2024-12-26 14:42:29 +0530

    form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -7,26 +7,26 @@ import { StepperProvider } from "../context/StepperContext";
 
 export const Register = ({ onCloseIconClick }) => {
   return (
-    <div className="relative font-robotoMono bg-[#ABD40F] w-[1485px] h-[884px] mx-auto flex px-[200px] z-1 rounded rounded-[10px]">
+    <div className="relative font-robotoMono bg-[#ABD40F] w-full sm:w-[1485px] h-[884px] mx-auto flex px-[40px] sm:px-[200px] z-1 rounded-[10px]">
       <img
         src={FormBg}
-        className="w-[100%] absolute left-0 opacity-[40%]"
+        className="w-full h-full absolute left-0 opacity-[40%] object-cover"
         alt="form background"
       />
       <img
         src={CodeCraftImage}
-        className="absolute right-[100px]"
+        className="absolute right-[50px] sm:right-[100px] bottom-[4px]"
         alt="code craft"
       />
       <img
         src={CloseIcon}
         onClick={onCloseIconClick}
-        className="absolute w-[21px] h-[21px] right-[24px] top-[24px] cursor-pointer"
+        className="absolute w-[21px] h-[21px] right-[20px] top-[20px] cursor-pointer sm:right-[24px] sm:top-[24px]"
         alt="close icon"
       />
       <StepperProvider>
         <Stepper />
-        <div className="absolute bottom-[50px] right-[400px]">
+        <div className="absolute bottom-[30px] sm:bottom-[50px] right-[30px] sm:right-[400px]">
           <StepperNavButton />
         </div>
       </StepperProvider>
