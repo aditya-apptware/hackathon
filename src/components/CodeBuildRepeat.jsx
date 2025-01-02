0dbcef18e33abe97311db6b14665cd792a738c12
aditya-apptware <aditya.arakal@apptware.com>
2025-01-02 12:51:21 +0530

    updated content for code build repeat

--- src/components/CodeBuildRepeat.jsx
+++ src/components/CodeBuildRepeat.jsx
@@ -38,25 +38,25 @@ export const CodeBuildRepeat = () => {
       />
 
       {/* Left Text */}
-      <div className="absolute text-white w-[90%] sm:w-[378px] h-[96px] text-[22px] leading-8 capitalize left-[5%] sm:left-[276px] top-[15%] sm:top-[307px] text-center sm:text-left hidden md:block">
-        &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-        elit libero&gt;
+      <div className="absolute text-white w-[90%] sm:w-[378px] h-[96px] text-[18px] leading-6 capitalize left-[5%] sm:left-[276px] top-[15%] sm:top-[307px] text-center sm:text-left hidden md:block">
+        Code drives innovation — it's the language of creativity and
+        transformation, shaping the future from ideation to execution.
       </div>
 
       {/* Right Text */}
-      <div className="absolute text-white w-[90%] sm:w-[407px] h-[96px] text-[22px] leading-8 capitalize right-[5%] sm:right-[324px] bottom-[15%] sm:bottom-[133px] text-center sm:text-right hidden md:block">
-        &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-        elit libero&gt;
+      <div className="absolute text-white w-[90%] sm:w-[407px] h-[96px] text-[18px] leading-6 capitalize right-[5%] sm:right-[324px] bottom-[15%] sm:bottom-[133px] text-center sm:text-left hidden md:block">
+        Repetition refines. By testing and enhancing, we create robust solutions
+        ready to make an impact.
       </div>
 
       {/* Below Text for Smaller Screens */}
-      <div className="md:hidden text-white text-center text-[22px] leading-8 capitalize mt-[20px] mx-auto max-w-[90%] sm:max-w-[378px] sm:max-w-[407px]">
-        &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-        elit libero&gt;
+      <div className="md:hidden text-white text-center text-[18px] leading-6 capitalize mt-[20px] mx-auto max-w-[90%] sm:max-w-[378px] sm:max-w-[407px]">
+        Code drives innovation — it's the language of creativity and
+        transformation, shaping the future from ideation to execution.
       </div>
-      <div className="md:hidden text-white text-center text-[22px] leading-8 capitalize mt-[20px] mx-auto max-w-[90%] sm:max-w-[378px] sm:max-w-[407px]">
-        &lt;Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-        elit libero&gt;
+      <div className="md:hidden text-white text-center text-[18px] leading-6 capitalize mt-[20px] mx-auto max-w-[90%] sm:max-w-[378px] sm:max-w-[407px]">
+        Repetition refines. By testing and enhancing, we create robust solutions
+        ready to make an impact.
       </div>
 
       <div className="flex flex-col space-y-2 hidden md:block">
