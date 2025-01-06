b1403be57c339fec503f567a2239dfe18031f2a4
aditya-apptware <aditya.arakal@apptware.com>
2025-01-06 16:54:51 +0530

    form fixes

--- src/components/forms/ConfirmationPopup.jsx
+++ src/components/forms/ConfirmationPopup.jsx
@@ -23,12 +23,15 @@ const ConfirmationPopup = ({ onCancel, onConfirm }) => {
   }, [formData]);
 
   return (
-    <div className="relative border border-black p-4 md:p-12 rounded-lg shadow-lg bg-gradient-to-br from-[#ABD40F] to-[#ABD40F] lg:w-[495px] h-[200px] md:h-[386px] opacity-90 mx-auto">
+    <div className="relative border border-black p-4 md:p-12 rounded-lg shadow-lg bg-gradient-to-br from-[#ABD40F] to-[#ABD40F] lg:w-[495px] opacity-90 mx-auto">
       <h2 className="font-bold mb-6 font-medium text-[18px] md:text-[32px] leading-[24px] md:leading-[45px]">
         Are you sure you want to submit the form?
       </h2>
-      <span>Note: Registraion fees for members will be collected after selection process!!</span>
-      <div className="flex justify-center gap-2 md:gap-8 absolute right-4 md:right-10 bottom-4 md:bottom-10 font-medium text-[16px] md:text-[22px] leading-[12px] md:leading-[22px]">
+      <p>
+        Note: Registraion fees for members will be collected after selection
+        process!!
+      </p>
+      <div className="flex justify-end mr-4 mb-4 gap-2 md:gap-8 font-medium text-[16px] md:text-[22px] leading-[12px] md:leading-[22px] mt-10">
         <button
           className="bg-[transparent] text-black px-4 py-2"
           onClick={onCancel}
